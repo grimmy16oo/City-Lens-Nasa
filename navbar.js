7e50f726ac28b5046323c3cc558e76a33764ba38
grimmy16oo <gsusmita655@gmail.com>
2025-08-26 17:34:13 +0600

    added9 idk

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 document.getElementById("index").addEventListener('click',function(e){
     e.preventDefault();
-    window.location.href = "./index.html";
+    window.location.href = "./dashboard.html";
 })
 document.getElementById("map-view").addEventListener('click',function(e){
     e.preventDefault();
