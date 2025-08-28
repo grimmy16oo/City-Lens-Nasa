1f27fc7243ee136ba291f4f2696962ea99c624b2
grimmy16oo <gsusmita655@gmail.com>
2025-08-29 00:06:14 +0600

    demo complete

---
+++ Js/navbar.js
@@ -0,0 +1,19 @@
+document.getElementById("nav-dashboard").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "../Html/index.html";
+});
+
+document.getElementById("nav-map").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "../Html/map.html";
+});
+
+document.getElementById("nav-analytics").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "../Html/analytics.html";
+});
+
+document.getElementById("nav-reports").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "../Html/reports.html";
+});
--- navbar.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("index").addEventListener('click',function(e){
-    e.preventDefault();
-    window.location.href = "./index.html";
-})
-document.getElementById("map-view").addEventListener('click',function(e){
-    e.preventDefault();
-    window.location.href = "./map.html";
-})
